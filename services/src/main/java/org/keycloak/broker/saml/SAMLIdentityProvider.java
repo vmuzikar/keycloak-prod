51cb54b42b8b29869330960319f394d14a4caf06
Luca Leonardo Scorcia <l.scorcia@gmail.com>
2020-09-23 18:08:49 -0400

    KEYCLOAK-15697 Make the Service Provider Entity ID user configurable
    (cherry picked from commit f274ec447bb8a665cd72beea969451743022763d)
    
    Conflicts:
      themes/src/main/resources/theme/base/admin/messages/admin-messages_en.properties

--- services/src/main/java/org/keycloak/broker/saml/SAMLIdentityProvider.java
+++ services/src/main/java/org/keycloak/broker/saml/SAMLIdentityProvider.java
@@ -132,7 +132,12 @@ public class SAMLIdentityProvider extends AbstractIdentityProvider<SAMLIdentityP
     }
 
     private String getEntityId(UriInfo uriInfo, RealmModel realm) {
-        return UriBuilder.fromUri(uriInfo.getBaseUri()).path("realms").path(realm.getName()).build().toString();
+        String configEntityId = getConfig().getEntityId();
+
+        if (configEntityId == null || configEntityId.isEmpty())
+            return UriBuilder.fromUri(uriInfo.getBaseUri()).path("realms").path(realm.getName()).build().toString();
+        else
+            return configEntityId;
     }
 
     @Override
